9b130a176ccc8b52ca08488c18203df7c420cb1a
iamsourav37 <souravganguly108@gmail.com>
2021-06-26 18:36:15 +0530

    user model and chat ui

---
+++ lib/models/UserModel.dart
@@ -0,0 +1,71 @@
+class UserModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatar;
+
+  UserModel(
+      {required this.name,
+      required this.message,
+      required this.time,
+      required this.avatar});
+}
+
+List<UserModel> usersData = [
+  UserModel(
+    name: "Ratul Rakshit",
+    message: "hello, how are you?",
+    time: "10:16",
+    avatar:
+        "https://images.unsplash.com/photo-1591258739299-5b65d5cbb235?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+  ),
+  UserModel(
+    name: "Amartya Chakraborty",
+    message: "see you soon",
+    time: "8:05",
+    avatar:
+        "https://images.unsplash.com/photo-1567515004624-219c11d31f2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
+  ),
+  UserModel(
+    name: "Rocket Kha",
+    message: "see you soon",
+    time: "8:05",
+    avatar:
+        "https://images.unsplash.com/photo-1611485988300-b7530defb8e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+  ),
+  UserModel(
+    name: "Soumen Shau",
+    message: "see you soon",
+    time: "8:05",
+    avatar:
+        "https://images.unsplash.com/photo-1608549036505-ead5b1de5417?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1900&q=80",
+  ),
+  UserModel(
+    name: "Arijit Dutta",
+    message: "see you soon",
+    time: "8:05",
+    avatar:
+        "https://images.unsplash.com/photo-1608549036505-ead5b1de5417?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1900&q=80",
+  ),
+  UserModel(
+    name: "Animesh Sarkar",
+    message: "see you soon",
+    time: "8:05",
+    avatar:
+        "https://images.unsplash.com/photo-1608549036505-ead5b1de5417?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1900&q=80",
+  ),
+  UserModel(
+    name: "Ujjawal",
+    message: "see you soon",
+    time: "8:05",
+    avatar:
+        "https://images.unsplash.com/photo-1608549036505-ead5b1de5417?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1900&q=80",
+  ),
+  UserModel(
+    name: "Abhishek",
+    message: "see you soon",
+    time: "8:05",
+    avatar:
+        "https://images.unsplash.com/photo-1608549036505-ead5b1de5417?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1900&q=80",
+  ),
+];
--- lib/screen/ChatScreen.dart
+++ lib/screen/ChatScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../models/UserModel.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -10,9 +11,60 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Chat screen"),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.chat,
+          color: Colors.white,
+        ),
+      ),
+      body: Container(
+        child: ListView.builder(
+          itemCount: usersData.length,
+          itemBuilder: (context, index) {
+            final user = usersData[index];
+            return Column(
+              children: [
+                Divider(
+                  height: 10,
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                  child: ListTile(
+                    onTap: () {
+                      print("${user.name} is tappped");
+                    },
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(user.avatar),
+                      radius: 30,
+                    ),
+                    title: Text(
+                      user.name,
+                      style: TextStyle(
+                        fontSize: 17.2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      user.message,
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    trailing: Text(
+                      user.time,
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
